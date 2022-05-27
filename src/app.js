2723dd4ff121666fe5c8f1b1ce3d93a1ab23e15f
Demi4nT <demhernandezmont21@gmail.com>
2022-05-27 01:27:50 +0000

    Segundo commit terminado

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 /* eslint-disable */
 import "./style.css";
 
-window.onload = function() {};
+window.onload = () => {
+  document.querySelector(".number").innerHTML = elnumero;
+  document.querySelector(".top-suit").innerHTML = lapinta;
+  document.querySelector(".bottom-suit").innerHTML = lapinta;
+};
 
 let numeros = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 let elnumero = numeros[Math.floor(Math.random() * numeros.length)];
-console.log(elnumero);
 
 let pintas = ["♥", "♠", "♣", "♦"];
 let lapinta = pintas[Math.floor(Math.random() * pintas.length)];
-console.log(lapinta);
+
+if (lapinta == "♥" || lapinta == "♦") {
+  document.getElementById("pinta1").style.color = "red";
+} else {
+  document.getElementById("pinta1").style.color = "black";
+}
+
+if (lapinta == "♥" || lapinta == "♦") {
+  document.getElementById("pinta2").style.color = "red";
+} else {
+  document.getElementById("pinta2").style.color = "black";
+}
